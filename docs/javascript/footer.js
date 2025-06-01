aad0bc7236f60915db9c73273e0fac6a25016110
Muhammad Talha <talha18513@gmail.com>
2025-06-01 19:13:24 +0500

    first added

---
+++ docs/javascript/footer.js
@@ -0,0 +1,67 @@
+const footer = document.getElementById('footer')
+footer.innerHTML = `
+    <footer class="px-5 pt-14 md:pt-20 bg-[#f7f7f7]">
+        <div class="max-w-7xl w-full mx-auto grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-20 gap-10 ">
+            <div class="w-full">
+                <h2 data-aos="fade-up" class="font-medium text-2xl  mb-3 text-[#214842]">About Us
+                </h2>
+                <p data-aos="fade-up" data-aos-delay="100" class="text-zinc-600 mt-5">We are supported with robust
+                    infrastructure, which is spread across a large area of land. We have fitted various advanced
+                    machines and tools in the unit that enable us in meeting customer demand on time.
+                </p>
+            </div>
+
+            <div class="w-full lg:pl-16">
+                <h2 data-aos="fade-up" class="font-medium text-2xl  mb-3 text-[#214842]">Quick Link</h2>
+                <div class="mt-7">
+                    <ul class="space-y-3 text-zinc-600">
+                        <li data-aos="fade-up"><a class="transition-all duration-500 hover:text-[#214842]"
+                                href="/privacy-policy.html">Home</a>
+                        </li>
+                        <li data-aos="fade-up" data-aos-delay="100"><a
+                                class="transition-all duration-500 hover:text-[#214842]" href="/terms-of-use.html">About
+                                Us </a></li>
+                        <li data-aos="fade-up" data-aos-delay="200"><a
+                                class="transition-all duration-500 hover:text-[#214842]" href="/products.html">Products
+                            </a></li>
+                        <li data-aos="fade-up" data-aos-delay="200"><a
+                                class="transition-all duration-500 hover:text-[#214842]"
+                                href="/testimonial.html">Testimonial </a></li>
+                        <li data-aos="fade-up" data-aos-delay="200"><a
+                                class="transition-all duration-500 hover:text-[#214842]" href="/contact.html">Contact Us
+                            </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="w-full">
+                <h2 data-aos="fade-up" class="font-medium text-2xl  mb-3 text-[#214842]">Contact Information</h2>
+                <div class="mt-7">
+                    <a data-aos="fade-up" href="#"
+                        class="flex items-start gap-3 transition-all mt-4 duration-500 hover:text-[#214842] text-zinc-600"><i
+                            class="fa-solid fa-location-dot text-xl text-zinc-500 mt-1"></i>Street No,
+                        CA 237 USA</a>
+                    <a data-aos="fade-up" href="mailto:info@demolink.org"
+                        class="flex items-center gap-3 transition-all mt-4 duration-500 hover:text-[#214842] text-zinc-600"><i
+                            class="fa-solid fa-envelope text-xl text-zinc-500 "></i>info@kundal.org</a>
+                    <div class="flex items-center gap-5 text-zinc-500 mt-8">
+                        <a data-aos="fade-up" href=""><i
+                                class="fa-brands transition-all duration-500 hover:text-[#214842] fa-facebook-f text-lg"></i></a>
+                        <a data-aos="fade-up" data-aos-delay="100" href=""><i
+                                class="fa-brands transition-all duration-500 hover:text-[#214842] fa-instagram text-xl"></i></a>
+                        <a data-aos="fade-up" data-aos-delay="200" href=""><i
+                                class="fa-brands transition-all duration-500 hover:text-[#214842] fa-youtube text-xl"></i></a>
+                        <a data-aos="fade-up" data-aos-delay="200" href=""><i
+                                class="fa-brands transition-all duration-500 hover:text-[#214842] fa-linkedin text-xl"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="max-w-7xl md:mt-16 mt-5 text-zinc-600 w-full mx-auto py-5 text-white text-center"> Kundal © <span
+                id="copyright"></span> All Rights
+            Reserved.</p>
+    </footer>
+`
+
+const year = new Date().getFullYear()
+const copyright = document.getElementById('copyright').innerHTML = year
---
+++ docs/javascript/navbar.js
@@ -0,0 +1,115 @@
+const navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+
+    <nav class="absolute top-0 left-0 w-full z-40 transition-all duration-300">
+        <div class="max-w-7xl mx-auto flex justify-between items-center w-full md:p-5 p-3 md:h-24 h-20">
+            <a data-aos="fade-right" href="./index.html" class="flex items-center justify-center lg:justify-start">
+                <img src="./docs/assets/images/LogoWhite.png" class="md:h-24 h-16" alt="logo">
+            </a>
+            <div data-aos="fade-left" class="md:flex hidden items-center gap-10">
+                <div class="md:flex items-center gap-10 hidden">
+                    <ul class="md:flex hidden items-center text-zinc-100 gap-10">
+                        <li><a href="/index.html" class="hover:text-[#cbffb4] transition-all duration-500">Home </a>
+                        </li>
+                        <li><a href="/about.html" class="hover:text-[#cbffb4] transition-all duration-500">About Us</a>
+                        </li>
+                        <li><a href="/products.html"
+                                class="hover:text-[#cbffb4] transition-all duration-500">Products</a></li>
+                        <li><a href="/testimonial.html"
+                                class="hover:text-[#cbffb4] transition-all duration-500">Testimonial</a></li>
+                    </ul>
+                </div>
+                <a href="/contact.html"
+                    class="block px-10 py-3 rounded-full font-medium transition-all duration-500 hover:opacity-80  text-white bg-[#214842]">Contact
+                    Us</a>
+            </div>
+
+            <i data-aos="fade-left"
+                class="fa-solid mobileMenuBtn text-zinc-200 fa-bars md:!hidden text-2xl cursor-pointer mr-2"></i>
+        </div>
+    </nav>
+
+
+    <nav id="nav" class="fixed bg-white -translate-y-[6rem] overflow-hidden top-0 left-0 w-full z-40 flex items-center justify-center transition-all duration-300">
+        <div class="max-w-7xl mx-auto flex justify-between items-center w-full md:p-5 p-3 md:h-24 h-20">
+            <a data-aos="fade-right" href="./index.html" class="flex items-center justify-center lg:justify-start">
+                <img src="./docs/assets/images/LogoBlack.png" class="md:h-20 h-16" alt="logo">
+            </a>
+            <div data-aos="fade-left" class="md:flex hidden items-center gap-10">
+                <div class="md:flex items-center gap-10 hidden">
+                    <ul  class="md:flex hidden items-center text-zinc-800 gap-10">
+                        <li><a href="/index.html" class="hover:text-[#214842] transition-all duration-500">Home </a>
+                        </li>
+                        <li><a href="/about.html" class="hover:text-[#214842] transition-all duration-500">About Us</a>
+                        </li>
+                        <li><a href="/products.html"
+                                class="hover:text-[#214842] transition-all duration-500">Products</a></li>
+                        <li><a href="/testimonial.html"
+                                class="hover:text-[#214842] transition-all duration-500">Testimonial</a></li>
+                    </ul>
+                </div>
+                <a href="/contact.html"
+                    class="block px-10 py-3 rounded-full font-medium transition-all duration-500 hover:opacity-80  text-white bg-[#214842]">Contact
+                    Us</a>
+            </div>
+
+            <i data-aos="fade-left" class="fa-solid mobileMenuBtn text-zinc-800 fa-bars md:!hidden text-2xl cursor-pointer mr-2"></i>
+        </div>
+    </nav>
+
+    <div id="mobileMenu"
+        class="fixed left-0 top-0 bg-white max-w-[30rem] w-0 overflow-hidden h-screen z-50 transition-all duration-500">
+        <div class="flex items-end justify-end p-5">
+            <i class="fa-solid mobileMenuCloseBtn fa-x z-50 text-zinc-700 text-xl cursor-pointer"></i>
+        </div>
+        <div class="p-6">
+            <ul class="flex relative z-50 text-zinc-800 flex-col gap-7">
+                <li><a href="/index.html"
+                        class="hover:text-[#f5e474] mobileMenuCloseBtn text-nowrap transition-all duration-500">Home</a>
+                </li>
+                <li><a href="/about.html"
+                        class="hover:text-[#f5e474] mobileMenuCloseBtn text-nowrap transition-all duration-500">About
+                        Us</a></li>
+                <li><a href="/products.html"
+                        class="hover:text-[#f5e474] mobileMenuCloseBtn text-nowrap transition-all duration-500">Products</a>
+                </li>
+                <li><a href="/testimonial.html"
+                        class="hover:text-[#f5e474] mobileMenuCloseBtn text-nowrap transition-all duration-500">Testimonial</a>
+                </li>
+            </ul>
+            <a href="/contact.html"
+                class="text-center block mobileMenuCloseBtn w-full p-3 border text-zinc-800 text-nowrap border-zinc-700 mt-5 rounded-lg cursor-pointer">Contact
+                us</a>
+        </div>
+    </div>
+`
+
+
+const mobileMenuBtn = document.querySelectorAll(".mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+const nav = document.getElementById("nav");
+const mobileMenuCloseBtns = document.querySelectorAll(".mobileMenuCloseBtn");
+
+mobileMenuCloseBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        mobileMenu.style.width = "0";
+    });
+});
+
+mobileMenuBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        mobileMenu.style.width = "100%";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 500) {
+            nav.style.transform = "translateY(0rem)";
+            nav.classList.remove("-translate-y-[6rem]")
+        } else {
+            nav.style.transform = "translateY(-6rem)";
+        }
+    });
+});
